5f11101d3715f56bb4a5f76d5fbfd9ae385750b9
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2023-08-29 00:24:41 -0400

    Updated and cleaned up script

--- .github/scripts/export_sheet.py
+++ .github/scripts/export_sheet.py
@@ -1,19 +1,59 @@
-import os
 import csv
+import os
+import sys
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from loguru import logger
 
-# Load service account credentials from environment variable
-credentials_json = os.environ['SERVICE_ACCOUNT_JSON']
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(eval(credentials_json))
 
-# Access the Google Sheet
-gc = gspread.authorize(credentials)
-sheet = gc.open_by_key(os.environ['SHEET_ID']).sheet1
+# 📝 Configure logging with loguru
+logger.add("export_sheet.log", rotation="10 MB")  # Log to file
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")  # Log to terminal
 
-# Export to CSV
-with open('data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(sheet.get_all_values())
+def get_env_variable(var_name, default=None):
+    """Retrieve environment variable safely."""
+    try:
+        var_value = os.environ[var_name]
+        if not var_value:
+            if default is not None:
+                return default
+            logger.error(f"❌ Environment variable {var_name} is empty.")
+            exit(1)
+        return var_value
+    except KeyError:
+        if default is not None:
+            return default
+        logger.error(f"❌ Environment variable {var_name} not set.")
+        exit(1)
 
-print("Exported Google Sheet to data.csv")
+# 🛠 Load service account credentials from environment variable
+try:
+    credentials_json = get_env_variable('SERVICE_ACCOUNT_JSON')
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(eval(credentials_json))
+except Exception as e:
+    logger.error(f"❌ Error loading service account credentials: {e}")
+    exit(1)
+
+# 📊 Access the Google Sheet
+try:
+    gc = gspread.authorize(credentials)
+    sheet_id = get_env_variable('SHEET_ID')
+    sheet = gc.open_by_key(sheet_id).sheet1
+except gspread.SpreadsheetNotFound:
+    logger.error(f"❌ Google Sheet with ID {sheet_id} not found.")
+    exit(1)
+except Exception as e:
+    logger.error(f"❌ Error accessing Google Sheet: {e}")
+    exit(1)
+
+# 💾 Export to CSV
+output_filename = get_env_variable('OUTPUT_FILENAME', default='data.csv')
+try:
+    with open(output_filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(sheet.get_all_values())
+    logger.info(f"✅ Exported Google Sheet to {output_filename}")
+except Exception as e:
+    logger.error(f"❌ Error exporting data to {output_filename}: {e}")
+    exit(1)
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# google-sheets-dump
+# 📊 Google Sheets to CSV GitHub Action Template
+
+This repository provides a GitHub Actions template to automatically export data from a Google Sheet to a CSV file and commit the changes to your GitHub repository. This is especially useful for projects that rely on data from Google Sheets and want to keep their repository updated with the latest data.
+
+## 🌟 Features:
+
+* 📝 Automated export of Google Sheet data to CSV.
+* 🚀 Commit and push changes to the repository.
+* ⏰ Schedule regular data pulls using cron syntax.
+
+## 🚀 Getting Started:
+
+### 1️⃣ Create Your Own Copy:
+
+To use this template:
+
+* Click on the `Use this template` button on the main page of this repository.
+* Provide a name for your new repository and click `Create repository from template`.
+
+### 2️⃣ Configure Secrets:
+
+In your newly created repository:
+
+* Navigate to `Settings` ➡️ `Secrets`.
+* Add the following secrets:
+  * `SHEET_ID`: The ID of your Google Sheet. (Hint: This can be found in the URL of your Google Sheet between `/spreadsheets/d/` and `/edit`).
+  * `SERVICE_ACCOUNT_JSON`: The content of the service account JSON key (🔐 make sure to wrap the entire JSON in single quotes).
+  * (Optional) `OUTPUT_FILENAME`: The desired name for the output CSV file. If not provided, the default will be `data.csv`.
+
+### 3️⃣ Set Up Regular Pulls:
+
+To set up regular pulls of information from the Google Sheet:
+
+* Visit [crontab.guru](https://crontab.guru/) to help you understand and generate the cron syntax for your desired schedule. 📅
+* Edit the `.github/workflows/sheet_to_csv.yml` file in your repository.
+* Under the `on` key, replace or add the `schedule` key with your desired cron schedule. For example, to run the action every day at 9 AM, you would add:
+ 
+    ```yaml
+    schedule:
+      - cron: '0 9 * * *'
+    ```
+
+## 📝 Final Notes:
+
+Ensure that the Google Sheet is shared with the email address of the service account (found in the JSON key) to grant it access. 🔒
+
+🎉 Enjoy automating your data workflow and keeping your repository up-to-date with the latest data from your Google Sheet!
